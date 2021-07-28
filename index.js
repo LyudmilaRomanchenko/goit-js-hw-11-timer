85fa79f1adaaf758f6cabdacf890db84bf3084b5
LyudmilaRomanchenko <78047438+LyudmilaRomanchenko@users.noreply.github.com>
2021-07-28 22:58:03 +0300

    Добавляет решение задачи таймер

--- index.js
+++ index.js
@@ -6,64 +6,98 @@ const refs = {
 }
 console.log(refs);
 
-const timer = {
-    start() {
-        const startTime = Date.now();
-        console.log(startTime);
+// Плагин для таймера
+class CountdownTimer {
+    constructor({selector, targetDate}) {
+            this.selector = selector;
+            this.targetDate = targetDate;
+            this.timeLeft();
+        }
 
+    timeLeft () {
+        const startTime = this.targetDate.getTime();
+        console.log(startTime);
         setInterval(() => {
             const curentTime = Date.now();
-            const deltaTime = curentTime - startTime;
+            const deltaTime = startTime - curentTime;
             const {days, hours, mins, secs} = getTimerComponents(deltaTime);
-            console.log(`${days}:${hours}:${mins}:${secs}`);
-           //showData();
-            refs.getDays.textContent = `0${days}`;
-            refs.getHours.textContent = `0${hours}`;
-            refs.getMins.textContent = `0${mins}`;
-            refs.getSecs.textContent = `0${secs}`;
+            // console.log(`${days}:${hours}:${mins}:${secs}`);
+            showData({days, hours, mins, secs});
         }, 1000);
-    }
+    }  
+}
 
+// Вызов экземпляра класса
+const timer = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Jul 31, 2021'),
+});
+
+console.log(timer);
+
+// Отображаем дату на странице
+function showData({days, hours, mins, secs}) {
+    refs.getDays.textContent = days;
+    refs.getHours.textContent = hours;
+    refs.getMins.textContent = mins;
+    refs.getSecs.textContent = secs;
 };
-// function showData() {
-//     //console.log(refs.getSecs);
-//     refs.getSecs.textContent = `0${secs}`;
-    
-// }
 
-//timer.start();
+// Показывает две цифры
+function pad(value) {
+    return String(value).padStart(2, '0')
+};
 
+// Получаем компоненты времени (дни, часы, минуты, секунды)
 function getTimerComponents(time) {
     
-//  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-//  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-//  */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    //  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+    //  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+    //  */
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    /*
+    * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+    * остатка % и делим его на количество миллисекунд в одном часе
+    * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+    */
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    /*
+    * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+    * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+    */
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    /*
+    * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+    * миллисекунд в одной секунде (1000)
+    */
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
     return {days, hours, mins, secs}
 }
 
-// 
 
-// const a = new CountdownTimer({
-//   selector: '#timer-1',
-//   targetDate: new Date('Jul 17, 2019'),
-// });
+
+
+
+//=========================
+
+// const timer = {
+//     start() {
+//         const startTime = Date.now();
+//         console.log(startTime);
+
+//         setInterval(() => {
+//             const curentTime = Date.now();
+//             const deltaTime = curentTime - startTime;
+//             const {days, hours, mins, secs} = getTimerComponents(deltaTime);
+           
+//             console.log(`${days}:${hours}:${mins}:${secs}`);
+//            showData({days, hours, mins, secs});
+        
+
+//         }, 1000);
+//     }
+
+// };
